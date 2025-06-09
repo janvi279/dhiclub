7dec527db288e115bc6ca1ec3580b46d29c053d6
janvi279 <janvi.alphabitinfoway@gmail.com>
2025-06-09 14:43:05 +0530

    fix login path

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./components/login";
+import Login from "./components/Login";
 import Dashboard from "./components/Dashboard";
 
 
