51641ab70b3609a5a26ef48320eb177870d35038
janvi279 <janvi.alphabitinfoway@gmail.com>
2025-06-14 12:12:09 +0530

    add user list  path

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -49,6 +49,12 @@ const Topbar = () => {
   <ul className="absolute top-15 right-0 mt-2 w-48 bg-white border border-gray-200 rounded-lg shadow-lg py-1 z-10">
     <li
       className="px-4 py-2 hover:bg-gray-200 cursor-pointer"
+      onClick={()=>navigate("/userList")}
+    >
+User List
+    </li>
+    <li
+      className="px-4 py-2 hover:bg-gray-200 cursor-pointer"
       onClick={handleLogout}
     >
       Logout
