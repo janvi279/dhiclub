37555103af71309c105a1d958614181b8346cf83
janvi279 <janvi.alphabitinfoway@gmail.com>
2025-09-08 03:15:58 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./pages/Auth/Login"
+import Login from "./pages/Auth/Login";
 import Home from "./pages/Home";
-import PageNotFound from "./pages/PageNotFound"
+import PageNotFound from "./pages/PageNotFound";
 import DefaultLayout from "./layout/DefaultLayout";
-import { Toaster } from 'react-hot-toast';
+import { Toaster } from "react-hot-toast";
 import AllRoutes from "./navigation";
-import PrivateRoute from "./components/custom/privateRoute"
+import PrivateRoute from "./components/custom/privateRoute";
 import PublicRoute from "./components/custom/privateRoutes";
 
 const App = () => {
@@ -15,28 +15,27 @@ const App = () => {
       <Toaster />
       {/* <AuthProvider> */}
       <Routes>
-        <Route path='/*' element={<PageNotFound />} />
-         <Route element={<PublicRoute />}>
-                  {/* <Route path='/signup' element={<Signup />} / */}
-                  <Route path='/login' element={<Login />} />
-                  {/* <Route path='/forgot-password' element={<ForgotPassword />} />
+        <Route path="/*" element={<PageNotFound />} />
+        <Route element={<PublicRoute />}>
+          {/* <Route path='/signup' element={<Signup />} / */}
+          <Route path="/login" element={<Login />} />
+          {/* <Route path='/forgot-password' element={<ForgotPassword />} />
                   <Route path='/reset-password' element={<ResetPassword />} /> */}
-                </Route>
+        </Route>
         <Route>
-        
           {/* <Route path='/register' element={<Register />} />
             <Route path='/forgot-password' element={<ForgotPassword />} />
             <Route path='/reset-password' element={<ResetPassword />} /> */}
         </Route>
         <Route>
           {/* <Route element={<PrivateRoute />}> */}
-            <Route element={<DefaultLayout />}>
-              {AllRoutes.map((route, index) => {
-                return (
-                  <Route key={index} path={route.path} element={route.element} />
-                )
-              })}
-            </Route>
+          <Route element={<DefaultLayout />}>
+            {AllRoutes.map((route, index) => {
+              return (
+                <Route key={index} path={route.path} element={route.element} />
+              );
+            })}
+          </Route>
           {/* </Route> */}
         </Route>
       </Routes>
